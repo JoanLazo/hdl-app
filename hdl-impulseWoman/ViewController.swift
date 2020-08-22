2d6b93e0f8fdf1f05d82db675c2a2008eef8fca4
hdl-womanimpulse.com <hdl-womanimpulse.com>
2020-08-22 11:30:50 -0500

    onboarding

--- hdl-impulseWoman/ViewController.swift
+++ hdl-impulseWoman/ViewController.swift
@@ -9,14 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    let button = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
-
-
 }
 
